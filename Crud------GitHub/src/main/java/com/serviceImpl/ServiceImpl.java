8faa4928b020709ea8ea3d2e4a9ccce404c0c412
shitalbelokar <belokarshital@gmail.com>
2018-10-25 20:05:20 +0530

    registerdata

--- Crud------GitHub/src/main/java/com/serviceImpl/ServiceImpl.java
+++ Crud------GitHub/src/main/java/com/serviceImpl/ServiceImpl.java
@@ -1,12 +1,23 @@
 package com.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.DaoI.DaoI;
+import com.model.Employee;
 import com.serviceI.ServiceI;
 
+@Service
 public class ServiceImpl implements ServiceI{
 	@Autowired
 	DaoI di;
+
+	@Override
+	public Employee registerEmployee(Employee e) {
+		System.out.println("in registeremploee");
+		Employee em=di.save(e);
+		return em;
+	}
+	
 
 }
